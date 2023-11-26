da98def0b6186174c0e182a007db74ec2ece10ff
Russell Matney <russell.matney@gmail.com>
2023-11-26 13:36:47 -0500

    wip: attempt to break out an aoc module
    Unfortunately modules don't seem to work with `zig run` (they require
    `zig build`), which is how i've been running each day's challenge, so
    this works with the main executable but not the daily aoc files. waa!

--- src/2015/day03_spherical_houses.zig
+++ src/2015/day03_spherical_houses.zig
@@ -1,46 +1,9 @@
 const std = @import("std");
+const aoc = @import("aoc");
 const pr = std.debug.print;
 const fs = std.fs;
-const http = std.http;
-const allocator = std.heap.page_allocator;
-
-const sesh = @embedFile("session.txt");
 
 const input_file = "day03_input.txt";
-
-fn fetch_input() !void {
-    var splits = std.mem.split(u8, sesh, "\n");
-    var session = splits.first();
-
-    var client = http.Client{
-        .allocator = allocator,
-    };
-
-    const uri = try std.Uri.parse("https://adventofcode.com/2015/day/3/input");
-    var headers = http.Headers{
-        .allocator = allocator,
-        .owned = false
-    };
-    try headers.append("accept", "*/*");
-    var cookie_str = try std.fmt.allocPrint(allocator, "session={s}", .{session});
-    try headers.append("cookie", cookie_str);
-
-    var req = try client.request(.GET, uri, headers, .{});
-    defer req.deinit();
-
-    try req.start();
-    try req.wait();
-
-    const body = req.reader().readAllAlloc(allocator, 8192) catch unreachable;
-    defer allocator.free(body);
-
-    var input_filename = "day03_input.txt";
-    var file = try fs.cwd().createFile(input_filename, .{});
-    defer file.close();
-    try file.writeAll(body);
-
-    pr("input data written to file {s}", .{input_filename});
-}
 
 pub fn main() !void {
     pr("[Day 3]", .{});
@@ -48,7 +11,7 @@ pub fn main() !void {
     fs.cwd().access(input_file, .{}) catch |err| switch (err) {
         error.FileNotFound => {
             pr("Input file not found, fetching...", .{});
-            try fetch_input();
+            try aoc.fetch_input(input_file);
             return err;
         },
         else => return err
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
+const aoc = @import("aoc");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    aoc.bleh();
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
