561f63ff902885fd07814e6100692ddb13a3cc6f
Russell Matney <russell.matney@gmail.com>
2023-12-01 17:48:16 -0500

    feat: 2023 day 01 part 2 complete!

--- src/2023/day01_trebuchet.zig
+++ src/2023/day01_trebuchet.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const mem = std.mem;
-const ArrayList = std.ArrayList;
+const StringHashMap = std.StringHashMap;
 const allocator = std.heap.page_allocator;
 
 const expectEqual = std.testing.expectEqual;
@@ -9,64 +9,70 @@ const parseInt = std.fmt.parseInt;
 
 const input = @embedFile("day01_input.txt");
 
-fn build_digits() !ArrayList([]const u8) {
-    var digits = ArrayList([]const u8).init(allocator);
-    try digits.append("1");
-    try digits.append("2");
-    try digits.append("3");
-    try digits.append("4");
-    try digits.append("5");
-    try digits.append("6");
-    try digits.append("7");
-    try digits.append("8");
-    try digits.append("9");
-    // try digits.append("one");
-    // try digits.append("two");
-    // try digits.append("three");
-    // try digits.append("four");
-    // try digits.append("five");
-    // try digits.append("six");
-    // try digits.append("seven");
-    // try digits.append("eight");
-    // try digits.append("nine");
+fn build_digits() !StringHashMap(u32) {
+    var digits = StringHashMap(u32).init(allocator);
+
+    try digits.put("1", 1);
+    try digits.put("2", 2);
+    try digits.put("3", 3);
+    try digits.put("4", 4);
+    try digits.put("5", 5);
+    try digits.put("6", 6);
+    try digits.put("7", 7);
+    try digits.put("8", 8);
+    try digits.put("9", 9);
+
+    try digits.put("one", 1);
+    try digits.put("two", 2);
+    try digits.put("three", 3);
+    try digits.put("four", 4);
+    try digits.put("five", 5);
+    try digits.put("six", 6);
+    try digits.put("seven", 7);
+    try digits.put("eight", 8);
+    try digits.put("nine", 9);
 
     return digits;
 }
 
-fn line_to_numbers(digits: ArrayList([]const u8), line: []const u8) !u32 {
+fn line_to_numbers(digits: StringHashMap(u32), line: []const u8) !u32 {
     var first_index = line.len;
-    var first_digit: []const u8 = "";
+    var first_digit: u32 = 0;
     var last_index: usize = 0;
-    var last_digit: []const u8 = "";
+    var last_digit: u32 = 0;
 
     if (line.len == 0) {
         return 0;
     }
 
-    for (digits.items) |digit| {
+    var it = digits.iterator();
+    while (it.next()) |entry| {
+        var digit = entry.key_ptr.*;
         var ix = mem.indexOf(u8, line, digit) orelse first_index;
         if (ix < first_index) {
             first_index = ix;
-            first_digit = digit;
+            first_digit = entry.value_ptr.*;
         }
     }
 
-    for (digits.items) |digit| {
+    var it2 = digits.iterator();
+    while (it2.next()) |entry| {
+        var digit = entry.key_ptr.*;
         var ix = mem.lastIndexOf(u8, line, digit) orelse last_index;
         if (ix > last_index) {
             last_index = ix;
-            last_digit = digit;
+            last_digit = entry.value_ptr.*;
         }
     }
 
-    if (mem.eql(u8, last_digit, "")) {
+    if (last_digit == 0) {
         last_digit = first_digit;
     }
 
-    pr("{c}", .{first_digit[0]});
-    pr("{c}\n", .{last_digit[0]});
+    pr("{d}", .{first_digit});
+    pr("{d}\n", .{last_digit});
 
-    var str_num = try std.fmt.allocPrint(allocator, "{c}{c}", .{first_digit[0], last_digit[0]});
+    var str_num = try std.fmt.allocPrint(allocator, "{d}{d}", .{first_digit, last_digit});
     var num = try parseInt(u32, str_num, 10);
 
     return num;
@@ -83,19 +89,18 @@ test "part 1" {
     try expectEqual(line_to_numbers(digits, "4dtlmkfnm"), 44);
 }
 
-// test "part 2" {
-//     var digits = try build_digits();
-//     defer digits.deinit();
+test "part 2" {
+    var digits = try build_digits();
+    defer digits.deinit();
 
-//     try expectEqual(line_to_numbers(digits, "two1nine"), 29);
-//     try expectEqual(line_to_numbers(digits, "eightwothree"), 83);
-//     try expectEqual(line_to_numbers(digits, "abcone2threexyz"), 13);
-//     try expectEqual(line_to_numbers(digits, "xtwone3four"), 24);
-//     try expectEqual(line_to_numbers(digits, "4nineeightseven2"), 42);
-//     try expectEqual(line_to_numbers(digits, "zoneight234"), 14);
-//     try expectEqual(line_to_numbers(digits, "7pqrstsixteen"), 76);
-// }
-
+    try expectEqual(line_to_numbers(digits, "two1nine"), 29);
+    try expectEqual(line_to_numbers(digits, "eightwothree"), 83);
+    try expectEqual(line_to_numbers(digits, "abcone2threexyz"), 13);
+    try expectEqual(line_to_numbers(digits, "xtwone3four"), 24);
+    try expectEqual(line_to_numbers(digits, "4nineeightseven2"), 42);
+    try expectEqual(line_to_numbers(digits, "zoneight234"), 14);
+    try expectEqual(line_to_numbers(digits, "7pqrstsixteen"), 76);
+}
 
 pub fn main() !void {
     pr("main running\n", .{});
