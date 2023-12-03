b75a40cd81d01738b6c8468b39e3535c3a7e9e5d
Russell Matney <russell.matney@gmail.com>
2023-12-02 22:06:45 -0500

    feat: day 2 parts 1+2

--- src/2023/day02_cube_conundrum.zig
+++ src/2023/day02_cube_conundrum.zig
@@ -12,8 +12,8 @@ const Game = struct {
     max_red: u32 = 0,
     max_green: u32 = 0,
     max_blue: u32 = 0,
-    fn power() u32 {
-        return .max_red * .max_blue * .max_green;
+    fn power(s: *Game) u32 {
+        return s.max_red * s.max_blue * s.max_green;
     }
 };
 
@@ -39,21 +39,25 @@ fn parse_max(line: []const u8, color: []const u8) !u32 {
     var it = std.mem.window([]const u8, parts.items, 2, 2);
     _ = it.next(); // Game id
 
+    var max: u32 = 0;
+
     while (it.next()) |pair| {
-        var c = pair[0];
-        var n = std.fmt.parseInt(u32, pair[1], 10);
-        if (std.mem.eql(u8, color, c)) {
-            return n;
+        var n = try std.fmt.parseInt(u32, pair[0], 10);
+        if (std.mem.eql(u8, color, pair[1])) {
+            if (n > max) {
+                max = n;
+            }
         }
     }
 
-    return 0;
+    return max;
 }
 
 test "parse_max" {
-    try expectEqual(parse_max("Game 3: red 14; blue 1; red 4", "red"), 14);
-    try expectEqual(parse_max("Game 3: red 9; blue 1; red 4", "blue"), 1);
-    try expectEqual(parse_max("Game 3: green 3, red 9; blue 1; red 4", "green"), 3);
+    try expectEqual(parse_max("Game 3: 14 red; 1 blue; 4 red", "red"), 14);
+    try expectEqual(parse_max("Game 3: 4 red; 1 blue; 14 red", "red"), 14);
+    try expectEqual(parse_max("Game 3: 9 red; 1 blue; 4 red", "blue"), 1);
+    try expectEqual(parse_max("Game 3: 3 green, 9 red; 1 blue; 4 red", "green"), 3);
 }
 
 fn parse_game(line: []const u8) !Game {
@@ -65,15 +69,41 @@ fn parse_game(line: []const u8) !Game {
     };
 }
 
+test "parse_game" {
+    var g = try parse_game("Game 3: 14 red; 1 blue; 4 red");
+    try expectEqual(g.id, 3);
+    try expectEqual(g.max_red, 14);
+    try expectEqual(g.max_green, 0);
+    try expectEqual(g.max_blue, 1);
+    try expectEqual(
+        parse_game("Game 3: 14 red; 1 blue; 4 red"),
+        Game{.id = 33, .max_red = 14, .max_blue = 1, .max_green = 0});
+}
+
 pub fn main() !void {
     var data = try aoc.input_data("2023", "2");
     var lines = split(u8, data, "\n");
-    var possible_games: u32 = 0;
+
+    var possible_game_ids: u32 = 0;
+    var total_power: u32 = 0;
+
+    var max_red: u32 = 12;
+    var max_green: u32 = 13;
+    var max_blue: u32 = 14;
 
     while (lines.next()) |line| {
+        if (line.len == 0) continue;
         var game = try parse_game(line);
-        pr("Game: {}\n", .{game});
+
+        if (game.max_red <= max_red
+            and game.max_green <= max_green
+                and game.max_blue <= max_blue) {
+            possible_game_ids += game.id;
+        }
+
+        total_power += game.power();
     }
 
-    pr("possible games: {}\n", .{possible_games});
+    pr("possible games: {}\n", .{possible_game_ids});
+    pr("total power: {}\n", .{total_power});
 }
